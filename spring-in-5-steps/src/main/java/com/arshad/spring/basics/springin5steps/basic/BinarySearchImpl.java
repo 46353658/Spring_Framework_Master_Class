88670317a7993266e20382e60639c7c843de809f
Arshad Aboo <arshad.aboo@entelect.co.za>
2019-04-25 12:04:50 +0200

    c->s5l26

--- spring-in-5-steps/src/main/java/com/arshad/spring/basics/springin5steps/basic/BinarySearchImpl.java
+++ spring-in-5-steps/src/main/java/com/arshad/spring/basics/springin5steps/basic/BinarySearchImpl.java
@@ -1,14 +1,21 @@
 package com.arshad.spring.basics.springin5steps.basic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class BinarySearchImpl {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
  	@Autowired
 	@Qualifier("quick")
@@ -20,6 +27,16 @@ public class BinarySearchImpl {
 		System.out.println(sortAlgorithm);
 		
 		return 3;
+	}
+
+	@PostConstruct
+	public void postConstruct() {
+		logger.info("postConstruct");
+	}
+
+	@PreDestroy
+	public void preDestroy() {
+		logger.info("preDestroy");
 	}
 
 }
