a5063f64f47700892fd5a9d8d4f90b0ac64b2426
Arshad Aboo <arshad.aboo@entelect.co.za>
2019-04-29 15:35:02 +0200

    c->s10l66

--- springboot-in-10-steps/src/main/java/com/arshad/springbootbasics/springbootin10steps/BooksController.java
+++ springboot-in-10-steps/src/main/java/com/arshad/springbootbasics/springbootin10steps/BooksController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BooksController {
 	@GetMapping("/books")
 	public List<Book> getAllBooks(){
-		return Arrays.asList(new Book(1l, "Mastering Spring 5.1", "Ranga"));
+		return Arrays.asList(new Book(1l, "Mastering Spring 5.66", "Ranga"));
 	}
 	
 }
