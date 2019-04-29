15de1c58feb89b08af7506ab39ee8014bcaa64c1
Arshad Aboo <arshad.aboo@entelect.co.za>
2019-04-29 17:00:01 +0200

    c->s10l67

---
+++ spring-aop/src/main/java/com/arshad/spring/aop/springaop/Dao2.java
@@ -0,0 +1,10 @@
+package com.arshad.spring.aop.springaop;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class Dao2 {
+	public String retrieveSomething() {
+		return "Dao2";
+	}
+}
