bd918f1f722d6566cef4fa0fc28def0749015c0a
Arshad Aboo <arshad.aboo@entelect.co.za>
2019-05-03 14:35:50 +0200

    c->s10l74

--- spring-aop/src/main/java/com/arshad/spring/aop/springaop/aspect/CommonJoinPointConfig.java
+++ spring-aop/src/main/java/com/arshad/spring/aop/springaop/aspect/CommonJoinPointConfig.java
@@ -8,4 +8,16 @@ public class CommonJoinPointConfig {
 	
 	@Pointcut("execution(* com.arshad.spring.aop.springaop.business.*.*(..))")
 	public void businessLayerExecution() {}
+	
+	@Pointcut("com.arshad.spring.aop.springaop.aspect.CommonJoinPointConfig.dataLayerExecution() && com.arshad.spring.aop.springaop.aspect.CommonJoinPointConfig.businessLayerExecution()")
+	public void allLayerExecution() {}
+	
+	@Pointcut("bean(dao*)")
+	public void beanStartingWithDao() {}
+	
+	@Pointcut("bean(*dao*)")
+	public void beanContainingDao() {}
+	
+	@Pointcut("within(com.arshad.spring.aop.springaop.data..*")
+	public void dataLayerExecutionWithWithin() {}
 }
