1c73651fc7fc82aee9be9e4569c4b67eda44e9a8
Arshad Aboo <arshad.aboo@entelect.co.za>
2019-05-03 16:31:52 +0200

    c->s11l82

---
+++ database-demo/src/main/java/com/arshad/database/databasedemo/jdbc/PersonJdbcDao.java
@@ -0,0 +1,23 @@
+package com.arshad.database.databasedemo.jdbc;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+
+import com.arshad.database.databasedemo.entity.Person;
+
+@Repository
+public class PersonJdbcDao {
+	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
+	// select * from person
+	public List<Person> findAll() {
+		return jdbcTemplate.query("select * from person", 
+				new BeanPropertyRowMapper(Person.class));
+	}
+}
