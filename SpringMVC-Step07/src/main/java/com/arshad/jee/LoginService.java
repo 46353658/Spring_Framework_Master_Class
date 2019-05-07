b6e2f7fa7f29f0329b176ab0c36cae70b0e3b296
Arshad Aboo <arshad.aboo@entelect.co.za>
2019-05-07 14:47:36 +0200

    c->s12l113

--- SpringMVC-Step07/src/main/java/com/arshad/jee/LoginService.java
+++ SpringMVC-Step07/src/main/java/com/arshad/jee/LoginService.java
@@ -1,4 +1,4 @@
-package webapp;
+package com.arshad.jee;
 
 public class LoginService {
 	public boolean validateUser(String user, String password) {
--- SpringMVC-Step07/src/main/java/com/arshad/jee/LoginServlet.java
+++ SpringMVC-Step07/src/main/java/com/arshad/jee/LoginServlet.java
@@ -1,4 +1,4 @@
-package webapp;
+package com.arshad.jee;
 
 import java.io.IOException;
 
